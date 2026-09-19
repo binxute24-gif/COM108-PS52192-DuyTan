31f0f3549d23804589d9a930c4c40b1445638e43
COM108-PS52192-DuyTan <binxute24@gmail.com>
2026-09-19 17:59:07 +0700

    Hoan thanh Lab1 Bai1

--- Lab1/bai3_tinhdiem.c
+++ Lab1/bai3_tinhdiem.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(){
-    char mssv[]="PS52184";
-    char hoten[]="nguyen thi cam nhung";
-    float diemtoan=8.0;
-    float diemly=7.5;
-    float diemhoa=8.5; 
-    float diemtrungbinh = (diemtoan + diemly + diemhoa)/3;
+    char mssv[]="PS52192";
+    char hoten[]="Nguyen Le Duy Tan";
+    float diemtoan=7.5;
+    float diemsu=8.0;
+    float diemdia=8.0; 
+    float diemtrungbinh = (diemtoan + diemsu + diemdia)/3;
     printf("ma so sinh vien: %s\n",mssv );
     printf("ho va ten: %s\n",hoten);
     printf("diem toan: %.2f\n",diemtoan);
-    printf("diem ly: %.2f\n",diemly);
-    printf("diem hoa: %.2f\n",diemhoa);
+    printf("diem su: %.2f\n",diemsu);
+    printf("diem dia: %.2f\n",diemdia);
     printf("diem trung binh: %.2f\n",diemtrungbinh);
     return 0;
 }
