87f559270bd8fcb3155754e00be053c354e6fc40
COM108-PS52192-DuyTan <binxute24@gmail.com>
2026-09-19 22:37:49 +0700

    Hoan thanh Lab1 Bai 1

--- Lab1/bai4_hoanvi.c
+++ Lab1/bai4_hoanvi.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main(){
-    int a=5;
-    int b=10;
-    printf("truoc khi hoan vi: a=%d , b = %d\n",a,b);
-    a = a+b;
-    b = a-b;
-    a = a-b;
-    printf("sau khi hoan vi: a = %d , b= %d\n",a,b);
+#define PI 3.14159
+  int main()
+{
+    const float PII = 3.14159;
+    float r = 5.5 , cv1, cv2, dt;
+    cv1 = r * 2 * PI;
+    cv2 = r * 2 * PII;
+
+    printf("Chu vi 1: %.2f\nChu vi 2: %.2f\n", cv1, cv2);
     return 0;
 }
